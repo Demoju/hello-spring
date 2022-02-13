2ebd4e6101e03176947e7b7eef0c0ae8e1573fbf
LeeMinJu <dmysonic@gmail.com>
2022-02-13 14:05:57 +0900

    스프링 웹 개발 기초 부분 업데이트

--- README.md
+++ README.md
@@ -2,6 +2,38 @@
 
 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근기술 강의를 따라하면서 작성한 내용입니다.
 
--   인프런 : [https//www.inflean.com](http://www.inflean.com)
+* 인프런 : [https//www.inflean.com](http://www.inflean.com)
+
+---
+
+### 프로젝트 생성하기
+
+***스프링부트 스타터*** 사이트로 이동하여 다음과 같이 스프링프로젝트를 생성.
+* [https://start.spring.io](https://start.spring.io)
+
+![](imgs/img.png)
+
+
+---
+
+### 프로젝트 import 및 실행하기
+
+유효한 단축키 
+
+    Ctrl + Shiht + F10 : Run Application
+    Shift + F10 : 최근 실행한 Run을 다시 실행하기 
+
+---
+
+### View 환경설정
+
+#### Welcome Page만들기
+
+resources/static/index.html
+
+![](imgs/img_1.png)
+
+
+
 
 
---
+++ src/main/java/com/demoju/hellospring/controller/HelloController.java
@@ -0,0 +1,29 @@
+package com.demoju.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("name", "Demoju");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public String helloApi(@RequestParam(value = "name", defaultValue = "default") String name) {
+        return "Hello~ " + name;
+    }
+}
