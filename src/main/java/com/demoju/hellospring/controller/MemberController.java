401fb118c01f450ff6fd189694d5e4f4ccd71644
LeeMinJu <dmysonic@gmail.com>
2022-02-13 21:43:25 +0900

    회원 가입 및 목록 기능 추가

--- src/main/java/com/demoju/hellospring/controller/MemberController.java
+++ src/main/java/com/demoju/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package com.demoju.hellospring.controller;
 
+import com.demoju.hellospring.domain.Member;
 import com.demoju.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -12,5 +18,29 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createForm";
+    }
+
+    @PostMapping("/members/new")
+    public String join(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setEmail(form.getEmail());
+        memberService.join(member);
+        return "redirect:/";
+
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+
     }
 }
